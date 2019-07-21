8a2a0cfdc5c467f895a06f2a937c2ecade7a6c5c
shotaro-ooe <shoooe97@gmail.com>
2019-07-21 09:55:59 +0900

    ログイン機能完成

---
+++ LineChat/LoginViewController.swift
@@ -0,0 +1,58 @@
+//
+//  LoginViewController.swift
+//  LineChat
+//
+//  Created by 大江祥太郎 on 2019/07/21.
+//  Copyright © 2019 shotaro. All rights reserved.
+//
+
+import UIKit
+import Firebase     //追加
+import GoogleSignIn//追加
+
+class LoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
+    //２つのデリゲートのプロトコルを追加
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* ここから追加 */
+        GIDSignIn.sharedInstance()?.uiDelegate = self
+        GIDSignIn.sharedInstance()?.delegate = self
+        /* ここまで追加 */
+    }
+    
+    /*** ここから追加！ ***/
+    
+    //チャット画面への遷移メソッド
+    func transitionToChatRoom() {
+        performSegue(withIdentifier: "toChatRoom", sender: self)//"toChatRoom"というIDで識別
+    }
+    
+    //Googleサインインに関するデリゲートメソッド
+    //signIn:didSignInForUser:withError: メソッドで、Google ID トークンと Google アクセス トークンを
+    //GIDAuthentication オブジェクトから取得して、Firebase 認証情報と交換します。
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        
+        
+        //最後に、認証情報を使用して Firebase での認証を行います
+        Auth.auth().signInAndRetrieveData(with: credential) { (authDataResult, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            print("\nLogin succeeded\n")
+            self.transitionToChatRoom()
+        }
+    }
+    
+    /*** ここまで追加！ ***/
+}
+
---
+++ Pods/Target Support Files/Pods-LineChat/Pods-LineChat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LineChat : NSObject
+@end
+@implementation PodsDummy_Pods_LineChat
+@end
